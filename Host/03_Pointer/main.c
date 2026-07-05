c61e3de0db3fc28e2f46548eea39da2383e95051
Animesh <un.animesh@gmail.com>
2026-07-05 19:36:49 +0530

    pointer

--- Host/03_Pointer/main.c
+++ Host/03_Pointer/main.c
@@ -15,10 +15,18 @@ int main(void)
 
 	printf("The value of address %p is: %X\n",pAddress, *pAddress);
 
-	pAddress = pAddress + 1;
+	pAddress = pAddress + 1; // it increment 1 byte of data bcz its char data type
 
 	printf("The value of address %p is: %X\n",pAddress, (unsigned char)*pAddress);
 	// type cast here bcz it df promoted to the int data types, df value is 11011111 it is negative number bcz of 1 in first.
+
+	 int *pAddressss = (int*)&g_data;
+
+	 printf("The value of address %p is %X\n",pAddressss, *pAddressss);
+
+	 pAddressss +=1;
+
+	 printf("The value of address %p is %X\n",pAddressss, *pAddressss);
 
 
 
