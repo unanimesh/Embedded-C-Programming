9dc3fafd9fe6a7c091db12f62b79398cc4c911c4
Animesh <un.animesh@gmail.com>
2026-07-09 20:25:51 +0530

    if statement

---
+++ Host/01_if_statement/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 9, 2026
+ *      Author: animesh
+ */
+
+//Write a program that takes the user’s age and decides whether a user can cast a vote or not.
+//
+//•The minimum age for casting a vote is 18 years
+//
+//•Print appropriate messages
+
+#include<stdio.h>
+int main()
+{
+	int age = 56;
+
+
+}
