6693593bd3ddfa52a7452feecbd3a3e8a7d76aa6
Animesh <un.animesh@gmail.com>
2026-07-11 17:33:34 +0530

    if-else-ladder statement

--- Host/01_if_else_ladder/main.c
+++ Host/01_if_else_ladder/main.c
@@ -18,19 +18,5 @@ int main()
 		printf("You have to pay %.2f$ taxes.\n",tax);
 	}
 
-	else if(38700 > income && income > 9525)
-	{
-		tax = (income*0)/100;
-		printf("You have to pay %.2f$ taxes.\n",tax);
-	}
-	else if(82500 > income && income > 38701)
-	{
-		tax = (income*22)/100;
-		printf("You have to pay %.2f$ taxes.\n",tax);
-	}
-	else if(income > 82501)
-	{
-		tax = ((income*32)/100)+1000;
-		printf("You have to pay %.2f$ taxes.\n",tax);
-	}
+
 }
