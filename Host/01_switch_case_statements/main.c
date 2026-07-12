7be3f6fc4b35dcf08f6103741036945fa9bea684
Animesh <un.animesh@gmail.com>
2026-07-12 14:40:13 +0530

    Switch case statement

--- Host/01_switch_case_statements/main.c
+++ Host/01_switch_case_statements/main.c
@@ -1,17 +1,14 @@
-/*
- * main.c
- *
- *  Created on: Jul 11, 2026
- *      Author: animesh
- */
-
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+double dimensions(const char *prompt);
 
 int main()
 {
 	char symbol;
-	double height, breadth, length, area;
+	int attempts = 0;
+	double length, breadth, height, area;
 
 	printf("Enter a symbol to find its area\n"
 		   "Triangle    --> 'T'\n"
@@ -20,16 +17,35 @@ int main()
 		   "Square      --> 'S'\n"
 		   "Rectangle   --> 'R'\n"
 		   "Please Enter any of the above Symbol:");
-	scanf("%c", &symbol);
+
+	while (attempts < 3)
+	{
+		if (scanf(" %c", &symbol) == 1)
+		{
+			if (symbol == 'T' || symbol == 't' || symbol == 'Z' || symbol == 'z' ||
+				symbol == 'C' || symbol == 'c' || symbol == 'S' || symbol == 's' ||
+				symbol == 'R' || symbol == 'r')
+			{
+				break; // Valid input, exit the loop
+			}
+			else
+			{
+				printf("Invalid input %d times. Please enter a valid symbol.\n", attempts + 1);
+				while (getchar() != '\n')
+					; // Clear the input buffer
+				attempts++;
+			}
+		}
+	}
+	// scanf("%c", &symbol);
 
 	switch (symbol)
 	{
 	case ('T'):
 	case ('t'):
-		printf("Enter the area breadth of the Triangle: ");
-		scanf("%lf", &breadth);
-		printf("Enter the height of the Triangle: ");
-		scanf("%lf", &height);
+
+		breadth = dimensions("Enter the breadth of the Triangle: ");
+		height = dimensions("Enter the height of the Triangle: ");
 
 		area = 0.5 * breadth * height;
 		printf("The area of Triangle is %lf\n", area);
@@ -37,12 +53,9 @@ int main()
 
 	case ('Z'):
 	case ('z'):
-		printf("Enter the shorter side of the Trapezoidal: ");
-		scanf("%lf", &breadth);
-		printf("Enter the longer side of the Trapezoidal: ");
-		scanf("%lf", &length);
-		printf("Enter the height of the Trapezoidal: ");
-		scanf("%lf", &height);
+		breadth = dimensions("Enter the shorter side of the Trapezoidal: ");
+		length = dimensions("Enter the longer side of the Trapezoidal: ");
+		height = dimensions("Enter the height of the Trapezoidal: ");
 
 		area = (0.5 * (breadth + length)) * height;
 		printf("The area of Trapezoidal is %lf\n", area);
@@ -50,8 +63,7 @@ int main()
 
 	case ('C'):
 	case ('c'):
-		printf("Enter the radius of the Circle: ");
-		scanf("%lf", &length);
+		length = dimensions("Enter the radius of the Circle: ");
 
 		area = 3.14 * length * length;
 		printf("The area of Circle is %lf\n", area);
@@ -59,8 +71,7 @@ int main()
 
 	case ('S'):
 	case ('s'):
-		printf("Enter the side of the Square: ");
-		scanf("%lf", &length);
+		length = dimensions("Enter the side of the Square: ");
 
 		area = length * length;
 		printf("The area of Square is %lf\n", area);
@@ -68,21 +79,37 @@ int main()
 
 	case ('R'):
 	case ('r'):
-		printf("Enter the length of the Rectangle: ");
-		scanf("%lf", &length);
-		printf("Enter the breadth of the Rectangle: ");
-		scanf("%lf", &breadth);
+		length = dimensions("Enter the length of the Rectangle: ");
+		breadth = dimensions("Enter the breadth of the Rectangle: ");
 
 		area = length * breadth;
 		printf("The area of Rectangle is %lf\n", area);
 		break;
-
-	default:
-		printf("Enter the correct symbol to find its area\n"
-			   "Triangle    --> 'T'\n"
-			   "Trapezoidal --> 'Z'\n"
-			   "Circle      --> 'C'\n"
-			   "Square      --> 'S'\n"
-			   "Rectangle   --> 'R'\n");
 	}
+}
+
+double dimensions(const char *prompt)
+{
+	double value;
+	char extra;
+	int attempts = 0;
+
+	while (attempts < 3)
+	{
+		printf("%s", prompt);
+		if (scanf("%lf%c", &value,&extra) != 1)
+		{
+			return value;
+		}
+		else
+		{
+			printf("Invalid input %d times. Please enter a numeric value.\n", attempts + 1);
+			while (getchar() != '\n')
+				; // Clear the input buffer
+			attempts++;
+		}
+		// break; // Exit the loop after 3 invalid attempts
+	}
+	printf("Too many invalid attempts. Exiting the program.\n");
+	exit(EXIT_FAILURE);
 }
