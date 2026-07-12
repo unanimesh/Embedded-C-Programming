ff14cc3c3b6da510ec907166556740cf0a9bc81a
Animesh <un.animesh@gmail.com>
2026-07-12 21:52:19 +0530

    Bitwise Operator

---
+++ Host/01_Bitwise_Operator/main.c
@@ -0,0 +1,38 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 12, 2026
+ *      Author: animesh
+ */
+
+#include<stdio.h>
+#include<stdint.h>
+
+int32_t input(char *prompt);
+
+int main() {
+	int32_t num1, num2, n1, n2;
+	num1 = input("Enter first number: ");
+	num2 = input("Enter second number: ");
+
+	printf("The first number is: %.8b\n", num1);
+	printf("The second number is: %.8b\n", num2);
+
+	n1 = num1;
+	n2 = num2;
+
+	printf("The bitwise AND of the two numbers is: %.8b which is %d\n", n1 & n2, n1 & n2);
+	printf("The bitwise OR of the two numbers is: %.8b which is %d\n", n1 | n2, n1 | n2);
+	printf("The bitwise XOR of the two numbers is: %.8b which is %d\n", n1 ^ n2, n1 ^ n2);
+	printf("The bitwise NOT of the %d is: %.8b which is %d\n", n1, ~n1, ~n1);
+
+	return 0;
+}
+
+int32_t input(char *prompt)
+{
+	int32_t num;
+	printf("%s", prompt);
+	scanf("%d", &num);
+	return num;
+}
