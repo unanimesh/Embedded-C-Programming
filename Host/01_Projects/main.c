ccdf7a4d6c19d21291b3d6fc8694a32a498b8651
Animesh <un.animesh@gmail.com>
2026-07-13 23:17:05 +0530

    projects

--- Host/01_Projects/main.c
+++ Host/01_Projects/main.c
@@ -2,8 +2,10 @@
 int main()
 {
 	int num;
-	printf("Enter the number:\n");
+	printf("Enter the Number\n");
 	scanf("%d",&num);
 
-	(num & 1)? printf("%d is Odd",num):printf("%d is Even",num);
+	(num & 1) ? printf("%d is Odd",num) : printf("%d is Even",num);
+
+	return 0;
 }
